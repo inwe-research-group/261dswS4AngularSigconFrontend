c11abeaa8f6f6e405a2b7f0450bb52f5c914d2ce
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-04-22 19:06:53 -0500

    add auth and toast service

---
+++ src/app/components/toast-container/toast-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-container',
+  imports: [],
+  templateUrl: './toast-container.html',
+  styleUrl: './toast-container.scss',
+})
+export class ToastContainer {
+
+}
