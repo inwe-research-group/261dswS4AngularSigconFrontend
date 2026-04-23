6aa920a01124ca90c19eee0d9f56b23c274b0899
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-04-22 19:33:16 -0500

    mejora login con auth and toast

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { ToastContainer } from './components/toast-container/toast-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer,ToastContainer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
