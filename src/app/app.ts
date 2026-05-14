6805071049761a8ed797f2c6e6355e877c0fc600
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-05-13 20:59:03 -0500

    reestructuracion del dashboard y app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
-import { ToastContainer } from './components/toast-container/toast-container';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer,ToastContainer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
