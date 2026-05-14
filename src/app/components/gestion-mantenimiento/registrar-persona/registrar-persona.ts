9486ded5c9343e6878be3e81e39c9ca38fd77cf4
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-05-13 20:36:50 -0500

    add 3 services and select control dinamico

--- src/app/components/gestion-mantenimiento/registrar-persona/registrar-persona.ts
+++ src/app/components/gestion-mantenimiento/registrar-persona/registrar-persona.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import { Component,inject,OnInit,ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
 import { PersonaResponse } from '../../../model/api/response/persona-response';
 import { PersonaRequest } from '../../../model/api/request/persona-request';
 import { PersonaService } from '../../../services/persona.service';
-
+import { TipoDocumentoService } from './../../../services/tipo-documento.service';
+import { UbigeoService } from '../../../services/ubigeo.service';
+import { SexoService } from '../../../services/sexo.service';
+import { TipoDocumento } from '../../../model/tipo-documento';
+import { Ubigeo } from '../../../model/ubigeo';
+import { Sexo } from '../../../model/sexo';
 
 @Component({
   selector: 'app-registrar-persona',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './registrar-persona.html',
   styleUrl: './registrar-persona.scss',
 })
-export class RegistrarPersona {
+export class RegistrarPersona implements OnInit{
   title="Registrar Persona";
+  private personaService = inject(PersonaService);
+  private tipoDocumentoService = inject(TipoDocumentoService);
+  private ubigeoService = inject(UbigeoService);
+  private sexoService = inject(SexoService);
+  private cdr = inject(ChangeDetectorRef);
+
   personaArray: PersonaResponse[]=[];
+  tipoDocumentoArray: TipoDocumento[]=[];
+  ubigeoArray: Ubigeo[]=[];
+  sexoArray: Sexo[]=[];
+
   personaRequest:PersonaRequest={} as PersonaRequest;
   personaForm:FormGroup;
 
-  constructor(
-    private personaService:PersonaService,
-  ){
+  constructor(){
     this.personaForm=new FormGroup({
       idPersona:new FormControl('',[Validators.required]),
       apellidoPaterno:new FormControl('',[Validators.required]),
@@ -36,6 +50,9 @@ export class RegistrarPersona {
   }//end del constructor
 
   ngOnInit(){
+    this.getSexo();
+    this.getTipoDocumento();
+    this.getUbigeo();
     this.getPersonas();
   }
 
@@ -44,6 +61,59 @@ export class RegistrarPersona {
       console.log(result);
       this.personaArray=result;
     });
+  }//end getPersonas()
+
+  getTipoDocumento():void{
+    this.tipoDocumentoService.getTipoDocumento().subscribe(
+    (result:TipoDocumento[])=>{
+      console.log(result);
+      this.tipoDocumentoArray=result;
+      this.cdr.detectChanges();
+    },
+    (err:any)=>{
+      console.log(err);
+    }
+  );
+  }//end getTipoDocumento()
+  setTipoDocumento(event: Event):void {
+    const inputChangeValue = (event.target as HTMLInputElement).value;
+    this.personaForm.controls['idTipoDocumento'].setValue(inputChangeValue);
+  }
+
+  getUbigeo():void{
+    this.ubigeoService.getUbigeo().subscribe(
+    (result:Ubigeo[])=>{
+      console.log(result);
+      this.ubigeoArray=result;
+      this.cdr.detectChanges();
+    },
+    (err:any)=>{
+      console.log(err);
+    }
+  );
+  }//end getUbigeo()
+
+  setUbigeo(event: Event):void {
+    const inputChangeValue = (event.target as HTMLInputElement).value;
+    this.personaForm.controls['idUbigeo'].setValue(inputChangeValue);
+  }
+
+  getSexo():void{
+    this.sexoService.getSexo().subscribe(
+    (result:Sexo[])=>{
+      console.log(result);
+      this.sexoArray=result;
+      this.cdr.detectChanges();
+    },
+    (err:any)=>{
+      console.log(err);
+    }
+  );
+  }//end getSexo()
+
+  setSexo(event: Event):void {
+    const inputChangeValue = (event.target as HTMLInputElement).value;
+    this.personaForm.controls['idSexo'].setValue(inputChangeValue);
   }
 
 
--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
@@ -10,7 +10,7 @@ import { PersonaResponse } from '../model/api/response/persona-response';
   providedIn: 'root',
 })
 export class PersonaService {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   getPersonas():Observable<PersonaResponse[]> {
     return this.http.get<PersonaResponse[]>(`${environment.url}/persona`);
