6ec4a95d3bf72fd9a3de040ca1c367047d7eb50c
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-06-03 18:49:43 -0500

    remove interceptor folder

--- src/app/interceptors/loading.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import {inject} from '@angular/core';
-import {HttpInterceptorFn} from '@angular/common/http'
-import { finalize } from 'rxjs/operators';
-
-/*export const loadingInterceptor: HttpInterceptorFn=(req,next)=>{
-
-};*/
