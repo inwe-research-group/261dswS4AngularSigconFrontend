d8f7eed8e59e55c08c45ad0ad31927297928c690
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-06-10 21:02:20 -0500

    add interceptor token and refreshToken

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,35 +1,53 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { UserRequest } from '../model/api/request/user-request';
 import { UserResponse } from '../model/api/response/user-response';
+import { Observable, tap } from 'rxjs';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private http = inject(HttpClient);
+  private storageService = inject(StorageService);
+
   private _isLoggedIn = signal<boolean>(this.hasToken());
   isLoggedIn = this._isLoggedIn.asReadonly();
 
-  constructor(private http: HttpClient) {}
-
-  login(user:UserRequest): Observable<UserResponse> {
+  login(user: UserRequest): Observable<UserResponse> {
     return this.http.post<UserResponse>(`${environment.url}/auth/login`, user);
   }
 
-  private hasToken(): boolean {
-    return !!localStorage.getItem('user_token');
+  refreshToken(): Observable<UserResponse> {
+    const refreshToken = this.storageService.getRefreshToken();
+    return this.http.post<UserResponse>(`${environment.url}/auth/refresh`, { refreshToken }).pipe(
+      tap((response: UserResponse) => {
+        this.setTokens(response.token, response.refreshToken);
+      })
+    );
   }
 
+
+  private hasToken(): boolean {
+    return !!this.storageService.getToken();
+  }
+
+  public setTokens(token: string, refreshToken: string) {
+    this.storageService.updateSession(token, refreshToken);
+    this._isLoggedIn.set(true);
+  }
+
+  // Mantengo setToken por compatibilidad si se usa en otros sitios
   public setToken(token: string) {
-    localStorage.setItem('user_token', token);
+    this.storageService.setItem('user_token', token);
     this._isLoggedIn.set(true);
   }
 
   logout() {
-    localStorage.removeItem('user_token');
+    this.storageService.removeItem('user_token');
+    this.storageService.removeCookie('refresh_token');
     this._isLoggedIn.set(false);
   }
-
 }
